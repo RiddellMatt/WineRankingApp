94a863ec66852341c0a8e37c031d49022d6b5b19
RiddellMatt <RiddellMatt@users.noreply.github.com>
2026-08-21 01:43:24 +0000

    deploy: 1d8f2956fefdba52069948811ff17acaa82031ac

--- assets/esm-DjZ5kexq.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-zP5R1fVR.js","assets/index-BiRvZPV1.js","assets/rolldown-runtime-C0FnF6B9.js","assets/index-B4IsqtpS.css"])))=>i.map(i=>d[i]);
-import{r as e,t}from"./index-BiRvZPV1.js";var n=e(`Preferences`,{web:()=>t(()=>import(`./web-zP5R1fVR.js`).then(e=>new e.PreferencesWeb),__vite__mapDeps([0,1,2,3]))});export{n as Preferences};
