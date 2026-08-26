9628c0a91b72ed12c465d57d0deff736852dff00
RiddellMatt <RiddellMatt@users.noreply.github.com>
2026-08-26 00:36:52 +0000

    deploy: d4133b2d2a671b5bab8e8e5e2fe9e7fbf0247105

--- assets/esm-BQ5MixT_.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Ih79XFHB.js","assets/index-CrlGmoHY.js","assets/rolldown-runtime-C0FnF6B9.js","assets/index-RGoLi5IP.css"])))=>i.map(i=>d[i]);
-import{r as e,t}from"./index-CrlGmoHY.js";var n=e(`Preferences`,{web:()=>t(()=>import(`./web-Ih79XFHB.js`).then(e=>new e.PreferencesWeb),__vite__mapDeps([0,1,2,3]))});export{n as Preferences};
---
+++ assets/web-C6_KY7xW.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-BWSNm6D6.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
