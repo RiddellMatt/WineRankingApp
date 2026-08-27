4dc635c85917918ce1f79fd8a2550bbd3e37fc41
RiddellMatt <RiddellMatt@users.noreply.github.com>
2026-08-27 01:58:13 +0000

    deploy: 29a8aa1155dcfbd335c3651383c09d64e4b0ff0f

---
+++ assets/esm-C-M3HJHY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DnL_dbjX.js","assets/index-BYuogFyx.js","assets/rolldown-runtime-C0FnF6B9.js","assets/index-BmG6vEZF.css"])))=>i.map(i=>d[i]);
+import{r as e,t}from"./index-BYuogFyx.js";var n=e(`Preferences`,{web:()=>t(()=>import(`./web-DnL_dbjX.js`).then(e=>new e.PreferencesWeb),__vite__mapDeps([0,1,2,3]))});export{n as Preferences};
